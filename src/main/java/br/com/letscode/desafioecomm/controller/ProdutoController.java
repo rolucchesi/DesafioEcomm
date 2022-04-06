a13343b3200becf88ec088941862ecfe833d9b39
Rodrigo da Cunha Lucchesi <rlucchesi92@gmail.com>
2022-04-06 14:16:25 -0300

    Melhoria Produto Controller

--- src/main/java/br/com/letscode/desafioecomm/controller/ProdutoController.java
+++ src/main/java/br/com/letscode/desafioecomm/controller/ProdutoController.java
@@ -1,14 +1,41 @@
 package br.com.letscode.desafioecomm.controller;
 
+import br.com.letscode.desafioecomm.dto.ProdutoDTO;
+import br.com.letscode.desafioecomm.model.ProdutoEntity;
 import br.com.letscode.desafioecomm.service.ProdutoService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @AllArgsConstructor
 @RequestMapping("produtos")
 @RestController
 public class ProdutoController {
     private ProdutoService produtoService;
+
+    @GetMapping()
+    public ResponseEntity<Page<ProdutoEntity>> get(
+            @RequestParam(name = "offset") Integer offset,
+            @RequestParam(name = "limit") Integer limit
+//            @RequestParam(name = "nome", required = false) String nome,
+//            @RequestParam(name = "valor_maximo", required = false) BigDecimal valorMaximo
+    ){
+        Page<ProdutoEntity> produtos = produtoService.buscarTodos(offset, limit);
+        return ResponseEntity.ok(produtos);
+    }
+
+    @PostMapping
+//    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ProdutoEntity> create(
+            @RequestBody ProdutoDTO request
+    ){
+        ProdutoEntity produto = produtoService.criar(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produto);
+    }
 
 }
